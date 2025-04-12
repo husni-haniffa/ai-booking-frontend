22a09e18822ff51249c1d2dd5d64c6a4dde748a5
Husni <husniwfayo@gmail.com>
2025-04-13 00:59:51 +0530

    key

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import store from "./lib/store";
 import { Provider } from "react-redux"
 import { ClerkProvider } from '@clerk/clerk-react'
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
