b735d4e7a5f6f9606fd818ad4b39eadf47719939
Husni <husniwfayo@gmail.com>
2025-04-12 22:40:49 +0530

    changed and updated clerk keys

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,15 @@ import store from "./lib/store";
 import { Provider } from "react-redux"
 import { ClerkProvider } from '@clerk/clerk-react'
 // Import your Publishable Key
-const PUBLISHABLE_KEY = "pk_test_YXNzdXJlZC1zZXJ2YWwtMi5jbGVyay5hY2NvdW50cy5kZXYk"
+const CLERK_PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY;
 
-if (!PUBLISHABLE_KEY) {
+if (!CLERK_PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+     <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} afterSignOutUrl="/">
         <Provider store={store}>
           <RouterProvider router={router} />
         </Provider>
